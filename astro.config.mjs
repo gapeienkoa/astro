6975f16a2f9b38d8b6e2bddba24386f71ff528e2
Artem Gapeienko <gapeienko.a@gmail.com>
2025-12-02 10:55:50 +0100

    add source

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ process.env.ASTRO_TELEMETRY_DISABLED = '1';
 
 // https://astro.build/config
 export default defineConfig({
+    /* https://docs.astro.build/en/guides/dev-toolbar/#per-project */
     devToolbar: { enabled: false }
 });
