25579502e6431d426a3535a723f4bab57cee86c6
Artem Gapeienko <gapeienko.a@gmail.com>
2025-12-02 02:13:33 +0100

    add desc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+/* https://github.com/withastro/astro/issues/9525 */
+/* https://github.com/withastro/astro/pull/12128 */
 process.env.ASTRO_TELEMETRY_DISABLED = '1';
 
 // https://astro.build/config
