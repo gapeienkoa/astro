274b13098ecd6072c33525130e63cfdc657f0ff2
Artem Gapeienko <gapeienko.a@gmail.com>
2025-12-02 02:12:32 +0100

    disable telemetry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+process.env.ASTRO_TELEMETRY_DISABLED = '1';
+
 // https://astro.build/config
 export default defineConfig({
     devToolbar: { enabled: false }
